410302f24e8c48791b31b8cf5e958ea6c8649dbe
AhmedMAyoub <ahmed2000mostafa@gmail.com>
2022-05-17 03:32:42 +0200

    Added stopwords removal, stemmer, and reading as text and body

--- SearchEngine/src/main/java/Indexer.java
+++ SearchEngine/src/main/java/Indexer.java
@@ -1,25 +1,19 @@
-import com.mongodb.*;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoClient;
-import com.mongodb.MongoClientSettings;
-import com.mongodb.ConnectionString;
-import com.mongodb.ServerAddress;
-import com.mongodb.MongoCredential;
-import com.mongodb.MongoClientOptions;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import com.mongodb.client.MongoDatabase;
 import org.jsoup.Jsoup;
-
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import ca.rmen.porterstemmer.PorterStemmer;
 import java.io.*;
-import java.net.URI;
-import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
-import java.net.UnknownHostException;
-
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 
 public class Indexer {
@@ -27,53 +21,89 @@ public class Indexer {
     public static final String DATABASE_NAME = "SearchEngineDB";
     public static final String DOCUMENTS_COLLECTION = "Documents";
 
+    public static final String STOP_WORD_FILE_PATH = "C:\\Users\\Ahmed\\Documents\\GitHub\\SearchEngine\\SearchEngine\\src\\main\\stopwords.txt";
+
+    public static final String DOWNLOADS_DIRECTORY = "C:\\Users\\Ahmed\\Documents\\GitHub\\SearchEngine\\SearchEngine\\src\\main\\Downloads\\";
+
     public static void main(String[] args) throws IOException, InterruptedException {
         MongoClient mongoClient = MongoClients.create(CONNECTION_STRING);
         MongoDatabase SearchEngineDb = mongoClient.getDatabase(DATABASE_NAME);
         MongoCollection<Document> collection = SearchEngineDb.getCollection(DOCUMENTS_COLLECTION);
         // access element at collection
-        downloadDocument("https://www.google.com", "google");
-        removeStopWords("google");
-
+        downloadDocument("https://www.webcotube.com/", "webco");
+        List<String> tokens = removeStopWords("webco");  // Tokens has each word in the document tokenized and without stop words
+        System.out.println(tokens);
+        tokens = stemmingTokens(tokens);    // Tokens stemmed
+        System.out.println(tokens);
     }
 
     public static void downloadDocument(String url, String title) throws IOException, InterruptedException {
         // download document from url as html
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .GET() // GET is default
-                .build();
+        org.jsoup.nodes.Document doc = Jsoup.connect(url).get();
+        File file = new File( DOWNLOADS_DIRECTORY + title + ".html");
 
-        HttpResponse<String> response = client.send(request,
-                HttpResponse.BodyHandlers.ofString());
-        URL url1 = new URL(url);
-        File file = new File( title + ".html");
         // write to a file.html
-        try(
-                BufferedReader reader = new BufferedReader(new InputStreamReader(url1.openStream()));
-                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        PrintWriter writer = new PrintWriter(file,"UTF-8");
+
+        // Code to remove all the inline html attributes and only keep tags
+        Elements el = doc.getAllElements();
+        for (Element e : el) {
+            List<String> attToRemove = new ArrayList<>();
+            Attributes at = e.attributes();
+            for (Attribute a : at) {
+                // transfer it into a list -
+                // to be sure ALL data-attributes will be removed!!!
+                attToRemove.add(a.getKey());
+            }
+
+            for(String att : attToRemove) {
+                e.removeAttr(att);
+            }
+        }
+
+        // write to file the html without attributes
+        writer.write(doc.body().toString());
+        writer.flush();
+        writer.close();
+    }
+
+    public static List<String> removeStopWords(String title) throws IOException {
+        // populate stop words in hash set
+        HashSet<String> stopWords = new HashSet<>();
+        File file = new File(STOP_WORD_FILE_PATH);
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(file));
         ) {
             String line;
             while ((line = reader.readLine()) != null) {
-                writer.write(line);
+                stopWords.add(line.toLowerCase());      // We always compare stop words in lower case
             }
         }
+
+        // read file and remove stop words
+        File file1 = new File(DOWNLOADS_DIRECTORY + title + ".html");
+        org.jsoup.nodes.Document doc = Jsoup.parse(file1, "UTF-8");     // Get the text only from the html file
+        // tokenize the text into string list and remove stop words
+        List<String> tokens = new ArrayList<>();
+        String text = doc.body().text();
+        String[] words = text.split(" ");
+        for (String word : words) {
+            if (!stopWords.contains(word.toLowerCase())) {
+                tokens.add(word);
+            }
+        }
+
+        return tokens;
     }
 
-    public static void removeStopWords(String title) throws IOException {
-        // remove stop words from the file
-        File file = new File(title + ".html");
-        // read file into a string
-        StringBuilder fileContents = new StringBuilder((int)file.length());
-        try(BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                fileContents.append(line);
-                fileContents.append(System.lineSeparator());
-            }
+    public static List<String> stemmingTokens(List<String> tokens) {
+        // stemming tokens using porter stemmer
+        List<String> stemmedTokens = new ArrayList<>();
+        PorterStemmer stemmer = new PorterStemmer();
+        for (String token : tokens) {
+            stemmedTokens.add(stemmer.stemWord(token.toLowerCase()));
         }
-        // remove stop words from the string
-        
+        return stemmedTokens;
     }
+
 }
