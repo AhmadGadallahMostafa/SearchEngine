784d31427184086bd4943d75732a604410dbbd29
Ahmad Gadallah <87261759+AhmadGadallah2000@users.noreply.github.com>
2022-05-17 17:55:34 +0200

    Inverted Index for multiple docs

--- SearchEngine/src/main/java/Indexer.java
+++ SearchEngine/src/main/java/Indexer.java
@@ -3,7 +3,6 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import com.mongodb.client.MongoDatabase;
-import org.javatuples.Pair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Attributes;
@@ -28,7 +27,7 @@ public class Indexer {
 
     public static final String DOWNLOADS_DIRECTORY = System.getProperty("user.dir") + "/src/main/Downloads/";
     public static HashSet<String> stopWords = new HashSet<>();
-    public static HashMap<String, HashMap<String, Integer>> InvertedIndex = new HashMap<>();
+    public static HashMap<String, HashMap<String, HashMap<String, Integer>>> InvertedIndex = new HashMap<>();
     public static final ArrayList<String> finalList = new ArrayList<String>(Arrays.asList("h1", "h2", "h3", "h4", "h5", "h6", "p", "title", "li", "a", "div"));
 
     public static void main(String[] args) throws IOException, InterruptedException {
@@ -36,9 +35,11 @@ public class Indexer {
         MongoDatabase SearchEngineDb = mongoClient.getDatabase(DATABASE_NAME);
         MongoCollection<Document> collection = SearchEngineDb.getCollection(DOCUMENTS_COLLECTION);
         // access element at collection
-        downloadDocument("https://www.geeksforgeeks.org/c-sharp-class-and-object/#:~:text=A%20class%20is%20a%20user,derived%20classes%20and%20base%20classes.", "ClassesInCSharp");
+        downloadDocument("https://www.geeksforgeeks.org/c-sharp-class-and-object/#:~:text=A%20class%20is%20a%20user,derived%20classes%20and%20base%20classes.", "geeksforgeeks");
+        downloadDocument("https://www.w3schools.com/cs/cs_classes.php", "w3s");
         stopWords = populateStopWords();
-        removeStopWords("ClassesInCSharp");  // Tokens has each word in the document tokenized and without stop words
+        removeStopWords("geeksforgeeks");  // Tokens has each word in the document tokenized and without stop words
+        removeStopWords("w3s");
         System.out.println(InvertedIndex);
     }
 
@@ -105,37 +106,38 @@ public class Indexer {
                 // check on the map if this is a new word
                 if (InvertedIndex.containsKey(stemmedWord))
                 {
-                    Integer oldFreq = InvertedIndex.get(stemmedWord).get("freq");
-                    InvertedIndex.get(stemmedWord).put("freq", oldFreq + 1);
-                    // check on the tag type of the word in the nested map
-                    if (InvertedIndex.get(stemmedWord).containsKey(element.tagName()))
+                    // get the first map
+                    HashMap<String, HashMap<String, Integer>> firstMap = InvertedIndex.get(stemmedWord);
+                    // check if the document title is in the inner map
+                    if (firstMap.containsKey(title))
                     {
-                        Integer oldTagFreq = InvertedIndex.get(stemmedWord).get(element.tagName());
-                        InvertedIndex.get(stemmedWord).put(element.tagName(), oldTagFreq + 1);
+                        // check on the tag
+                        HashMap<String, Integer> secondMap = firstMap.get(title);
+                        // check if the tag has this word
+                        if (secondMap.containsKey(element.tagName()))
+                            secondMap.put(element.tagName(), secondMap.get(element.tagName()) + 1);
+                        else
+                            secondMap.put(element.tagName(), 1);                            // add the word to the tag
                     }
                     else
                     {
-                        InvertedIndex.get(stemmedWord).put(element.tagName(), 1);
+                        // add the document title to the map
+                        HashMap<String, Integer> secondMap = new HashMap<>();
+                        secondMap.put(element.tagName(), 1);
+                        firstMap.put(title, secondMap);
                     }
-                } else {
-                    HashMap<String, Integer> tagFreq = new HashMap<>();
-                    tagFreq.put(element.tagName(), 1);
-                    InvertedIndex.put(stemmedWord, tagFreq);
-                    // insert in freq key the value 1
-                    InvertedIndex.get(stemmedWord).put("freq", 1);
+                }
+                else
+                {
+                    // add the word to the map
+                    HashMap<String, HashMap<String, Integer>> firstMap = new HashMap<>();
+                    HashMap<String, Integer> secondMap = new HashMap<>();
+                    secondMap.put(element.tagName(), 1);
+                    firstMap.put(title, secondMap);
+                    InvertedIndex.put(stemmedWord, firstMap);
                 }
             }
         }
-    }
-
-    public static List<String> stemmingTokens(List<String> tokens) {
-        // stemming tokens using porter stemmer
-        List<String> stemmedTokens = new ArrayList<>();
-        PorterStemmer stemmer = new PorterStemmer();
-        for (String token : tokens) {
-            stemmedTokens.add(stemmer.stemWord(token.toLowerCase()));
-        }
-        return stemmedTokens;
     }
 
     public static HashSet<String> populateStopWords()
