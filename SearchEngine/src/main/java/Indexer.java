2e7ca0d953285f1190f260bcc8d9f71043205345
Ahmad Gadallah <87261759+AhmadGadallah2000@users.noreply.github.com>
2022-05-17 14:52:52 +0200

    Inverted Index for a single document

--- SearchEngine/src/main/java/Indexer.java
+++ SearchEngine/src/main/java/Indexer.java
@@ -3,6 +3,7 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
 import com.mongodb.client.MongoDatabase;
+import org.javatuples.Pair;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Attributes;
@@ -12,6 +13,8 @@ import ca.rmen.porterstemmer.PorterStemmer;
 import java.io.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
@@ -24,18 +27,19 @@ public class Indexer {
     public static final String STOP_WORD_FILE_PATH = System.getProperty("user.dir") + "/src/main/stopwords.txt";
 
     public static final String DOWNLOADS_DIRECTORY = System.getProperty("user.dir") + "/src/main/Downloads/";
+    public static HashSet<String> stopWords = new HashSet<>();
+    public static HashMap<String, HashMap<String, Integer>> InvertedIndex = new HashMap<>();
+    public static final ArrayList<String> finalList = new ArrayList<String>(Arrays.asList("h1", "h2", "h3", "h4", "h5", "h6", "p", "title", "li", "a", "div"));
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println(STOP_WORD_FILE_PATH);
         MongoClient mongoClient = MongoClients.create(CONNECTION_STRING);
         MongoDatabase SearchEngineDb = mongoClient.getDatabase(DATABASE_NAME);
         MongoCollection<Document> collection = SearchEngineDb.getCollection(DOCUMENTS_COLLECTION);
         // access element at collection
-        downloadDocument("https://www.webcotube.com/", "webco");
-        List<String> tokens = removeStopWords("webco");  // Tokens has each word in the document tokenized and without stop words
-        System.out.println(tokens);
-        tokens = stemmingTokens(tokens);    // Tokens stemmed
-        System.out.println(tokens);
+        downloadDocument("https://www.geeksforgeeks.org/c-sharp-class-and-object/#:~:text=A%20class%20is%20a%20user,derived%20classes%20and%20base%20classes.", "ClassesInCSharp");
+        stopWords = populateStopWords();
+        removeStopWords("test");  // Tokens has each word in the document tokenized and without stop words
+        System.out.println(InvertedIndex);
     }
 
     public static void downloadDocument(String url, String title) throws IOException, InterruptedException {
@@ -49,6 +53,11 @@ public class Indexer {
         // Code to remove all the inline html attributes and only keep tags
         Elements el = doc.getAllElements();
         for (Element e : el) {
+            // check if element is a script or style tag remove it
+            if (e.tagName().equals("script") || e.tagName().equals("style")) {
+                e.remove();
+                continue;
+            }
             List<String> attToRemove = new ArrayList<>();
             Attributes at = e.attributes();
             for (Attribute a : at) {
@@ -68,33 +77,55 @@ public class Indexer {
         writer.close();
     }
 
-    public static List<String> removeStopWords(String title) throws IOException {
-        // populate stop words in hash set
-        HashSet<String> stopWords = new HashSet<>();
-        File file = new File(STOP_WORD_FILE_PATH);
-        try (
-                BufferedReader reader = new BufferedReader(new FileReader(file));
-        ) {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stopWords.add(line.toLowerCase());      // We always compare stop words in lower case
-            }
-        }
-
+    public static void removeStopWords(String title) throws IOException {
         // read file and remove stop words
-        File file1 = new File(DOWNLOADS_DIRECTORY + title + ".html");
-        org.jsoup.nodes.Document doc = Jsoup.parse(file1, "UTF-8");     // Get the text only from the html file
-        // tokenize the text into string list and remove stop words
-        List<String> tokens = new ArrayList<>();
-        String text = doc.body().text();
-        String[] words = text.split(" ");
-        for (String word : words) {
-            if (!stopWords.contains(word.toLowerCase())) {
-                tokens.add(word);
+        File htmlFile = new File(DOWNLOADS_DIRECTORY + title + ".html");
+        org.jsoup.nodes.Document doc = Jsoup.parse(htmlFile, "UTF-8");
+        // looping over elements of the html doc and extracting words then stemming it then putting it the inverted index map
+        Elements elements = doc.getAllElements();
+        PorterStemmer stemmer = new PorterStemmer();
+        for (Element element: elements)
+        {
+            // We only want to index the words in the finalList tags
+            if (!finalList.contains(element.tagName()))
+                continue;
+            // to avoid getting nested word of elements
+            String text = element.ownText();
+            if (text.isEmpty())
+                continue;
+
+            // split the text into words in a seperate list
+            String[] words = text.split(" ");
+            for (String word : words)
+            {
+                String stemmedWord = stemmer.stemWord(word);
+                // if the word is a stop word then skip it
+                if (stopWords.contains(stemmedWord))
+                    continue;
+                // check on the map if this is a new word
+                if (InvertedIndex.containsKey(stemmedWord))
+                {
+                    Integer oldFreq = InvertedIndex.get(stemmedWord).get("freq");
+                    InvertedIndex.get(stemmedWord).put("freq", oldFreq + 1);
+                    // check on the tag type of the word in the nested map
+                    if (InvertedIndex.get(stemmedWord).containsKey(element.tagName()))
+                    {
+                        Integer oldTagFreq = InvertedIndex.get(stemmedWord).get(element.tagName());
+                        InvertedIndex.get(stemmedWord).put(element.tagName(), oldTagFreq + 1);
+                    }
+                    else
+                    {
+                        InvertedIndex.get(stemmedWord).put(element.tagName(), 1);
+                    }
+                } else {
+                    HashMap<String, Integer> tagFreq = new HashMap<>();
+                    tagFreq.put(element.tagName(), 1);
+                    InvertedIndex.put(stemmedWord, tagFreq);
+                    // insert in freq key the value 1
+                    InvertedIndex.get(stemmedWord).put("freq", 1);
+                }
             }
         }
-
-        return tokens;
     }
 
     public static List<String> stemmingTokens(List<String> tokens) {
@@ -105,6 +136,24 @@ public class Indexer {
             stemmedTokens.add(stemmer.stemWord(token.toLowerCase()));
         }
         return stemmedTokens;
+    }
+
+    public static HashSet<String> populateStopWords()
+    {
+        // populate stop words in hash set
+        HashSet<String> stopWords = new HashSet<>();
+        File file = new File(STOP_WORD_FILE_PATH);
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(file));
+        ) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stopWords.add(line.toLowerCase());      // We always compare stop words in lower case
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return stopWords;
     }
 
 }
