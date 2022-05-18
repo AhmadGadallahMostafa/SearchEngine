66db158048e77fc05c11f1a1444a2f70baa0f87e
manaarr <manaarrzakaria@gmail.com>
2022-05-18 02:47:31 +0200

    search page & json

--- search-engine/src/Home.js
+++ search-engine/src/Home.js
@@ -2,11 +2,13 @@ import './Home.css';
 import {useState} from 'react';
 import logo from './logo.png';
 import search from './search.png'; 
+import { useNavigate } from "react-router-dom";
 
 function Home() {
     const [text, setText] = useState('');
+    let navigate = useNavigate();
     const handleClick = () => {
-        console.log(text);
+        navigate(`/search?q=${text}`);
     }
     return (
         <div className="App">
--- search-engine/src/Search.js
+++ search-engine/src/Search.js
@@ -1,7 +1,42 @@
+import logo from './logo.png';
+import './Search.css';
+import search from './search.png';
+import { useNavigate } from "react-router-dom";
+import {useState, useEffect} from 'react';
+
 function Search () {
+    const [text, setText] = useState('taylor');
+    const [links, setLinks] = useState(null);
+    let navigate = useNavigate();
+    const handleClick = () => {
+        navigate(`/search?q=${text}`);
+    }
+    useEffect(() => {
+        fetch('http://localhost:8000/Links')
+        .then(res => {
+            return res.json();
+        })
+        .then(data => {
+            setLinks(data);
+        });
+    }, []);
     return (
-        <div className="Search">
-        <h1>Search</h1>
+        <div className="search">
+        <div className="search-header">
+            <div className="image-container">
+                <img className="search-logo"src= {logo} alt="linkdig"/>
+            </div>
+            <input className= "search-bar-nav" type="text" onChange={(e) => setText(e.target.value)} placeholder = {text}/>
+            <img className = "search-button-nav" src={search} alt="search"  onClick = {() => {handleClick()}}/>
+        </div>
+        {links && links.map(link => {
+            return (
+                <div className="search-container" key ={link.url}>
+                    <div className="search-title">{link.title}</div>
+                    <div className="search-description">{link.description}</div>
+                </div>
+            )
+        })}
         </div>
     );
 }
