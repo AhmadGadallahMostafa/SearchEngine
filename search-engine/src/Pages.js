8f7e7ef36348673a21def361691edc91b6d4044c
manaarr <manaarrzakaria@gmail.com>
2022-05-18 14:30:26 +0200

    draft pagination

---
+++ search-engine/src/Pages.js
@@ -0,0 +1,19 @@
+import React from "react";
+import './Search.css';
+
+function Links(props) {
+return (
+    <div className="links">
+    {props.links.map((link) => {
+    return (
+        <div className="search-container" key ={link.url}>
+            <a href={link.url} target="_blank" className="search-title">{link.title}</a>
+            <div className="search-description">{link.description}</div>
+        </div>
+    )
+    })}
+    </div>
+)
+}
+
+export default Links;
--- search-engine/src/Search.js
+++ search-engine/src/Search.js
@@ -1,16 +1,23 @@
 import logo from './logo.png';
 import './Search.css';
 import search from './search.png';
+import ReactPaginate from 'react-paginate';
+import Links from './Pages';
 import { useNavigate } from "react-router-dom";
 import {useState, useEffect} from 'react';
 
 function Search () {
     const [text, setText] = useState('');
-    const [links, setLinks] = useState(null);
+    const [links, setLinks] = useState([]);
+    const [pageCount, setpageCount] = useState(0);
+
+    let per_page = 10;
     let navigate = useNavigate();
+
     const handleClick = () => {
         navigate(`/search?q=${text}`);
     }
+
     useEffect(() => {
         let t = window.location.search.split('=')[1];
         if (t.indexOf('%20') === -1) {
@@ -19,14 +26,33 @@ function Search () {
         else{
             setText(t.split('%20').join(' '));
         }
-        fetch('http://localhost:8000/Links')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
+        const getLinks = async () => {
+            const res = await fetch(
+                `http://localhost:8000/Links?_page=1&_per_page=${per_page}`
+            );
+            const data = await res.json();
+            const total = res.headers.get("x-total-count");
+            setpageCount(Math.ceil(total / per_page));
             setLinks(data);
-        });
-    }, []);
+        }
+        getLinks();
+    }, [per_page]);
+
+    const fetchLinks = async (currentPage) => {
+        const res = await fetch(
+          `http://localhost:8000/Links?_page=${currentPage}&_per_page=${per_page}`
+        );
+        const data = await res.json();
+        return data;
+      };
+
+    const handlePageClick = async (data) => {
+        let currentPage = data.selected + 1;
+        console.log(currentPage);
+        const linksFormServer = await fetchLinks(currentPage);
+        setLinks(linksFormServer);
+    };
+
     return (
         <div className="search">
         <div className="search-header">
@@ -35,19 +61,31 @@ function Search () {
             </div>
             <input className= "search-bar-nav" type="text" onKeyPress={(e) => {
                         if (e.key === "Enter") {
-                            console.log("ana enter");
                             handleClick();
                         }}} onChange={(e) => setText(e.target.value)} value = {text}/>
             <img className = "search-button-nav" src={search} alt="search"  onClick = {() => {handleClick()}}/>
         </div>
-        {links && links.map(link => {
-            return (
-                <div className="search-container" key ={link.url}>
-                    <a href={link.url} target="_blank" className="search-title">{link.title}</a>
-                    <div className="search-description">{link.description}</div>
-                </div>
-            )
-        })}
+        {console.log(links)}
+        <Links links={links}/>
+        <ReactPaginate
+            previousLabel={"previous"}
+            nextLabel={"next"}
+            breakLabel={"..."}
+            pageCount={pageCount}
+            marginPagesDisplayed={2}
+            pageRangeDisplayed={3}
+            onPageChange={handlePageClick}
+            containerClassName={"pagination justify-content-center"}
+            pageClassName={"page-item"}
+            pageLinkClassName={"page-link"}
+            previousClassName={"page-item"}
+            previousLinkClassName={"page-link"}
+            nextClassName={"page-item"}
+            nextLinkClassName={"page-link"}
+            breakClassName={"page-item"}
+            breakLinkClassName={"page-link"}
+            activeClassName={"active"}
+        />
         </div>
     );
 }
