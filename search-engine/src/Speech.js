5555adfa89988daf4832fc7b6155daf92c80ed6c
manaarr <manaarrzakaria@gmail.com>
2022-05-19 03:26:08 +0200

    Speech Recognition
    bugs in text input

---
+++ search-engine/src/Speech.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { useState } from 'react';
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
+import error from './error.png';
+import './Speech.css';
+import micIconDis from './micDis.png';
+import micIconAct from './micAct.png';
+
+function Speech({handleSpeech}) {
+    /*initializing the speech recognition*/
+    const [isListening, setIsListening] = useState(false);
+    const [isActive, setIsActive] = useState("mic-dis");
+    const [micIcon, setMicIcon] = useState(micIconDis);
+    const { transcript } = useSpeechRecognition();
+    const isSupported = SpeechRecognition.browserSupportsSpeechRecognition();
+
+    const handleClick = () => {
+        setIsListening(!isListening);
+        if (isListening) {
+            setIsActive("mic-dis");
+            setMicIcon(micIconDis);
+            SpeechRecognition.stopListening();
+        }
+        else {
+            setIsActive("mic-act");
+            setMicIcon(micIconAct);
+            SpeechRecognition.startListening({ continuous: true, interimResults: true });
+        }
+    };
+
+    return (
+        < div className="speech-container" >
+            {!isSupported ? (
+                <div className="speech-container">
+                    <img src={error} alt="error" className="error-img" />
+                </div>
+            ) : (
+                <div>
+                    <div className="speech-area">
+                        <img src={micIcon} alt="microphone" className={isActive} id="mic-icon" onClick={handleClick} />
+                    </div>
+                    <p>{transcript ? handleSpeech(transcript) : handleSpeech('')}</p>
+                </div>
+            )
+            }
+        </div >
+    );
+}
+
+export default Speech;
--- search-engine/src/index.js
+++ search-engine/src/index.js
@@ -3,17 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Home from './Home';
 import Search from './Search';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'; 
+import Speech from './Speech';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/" element={<Home />} />
       </Routes>
       <Routes>
-        <Route exact path="/search" element={<Search/>}/>
+        <Route exact path="/search" element={<Search />} />
+      </Routes>
+      <Routes>
+        <Route exact path="/speech" element={<Speech />} />
       </Routes>
     </Router>
   </React.StrictMode>
